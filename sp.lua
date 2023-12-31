b8611c4a89d5a9df2cf6bc66c62252fb402cc285
seven <786852516@qq.com>
2023-12-31 22:23:17 +0800

    曹昂 微调

--- sp.lua
+++ sp.lua
@@ -1757,16 +1757,20 @@ local kangkai = fk.CreateTriggerSkill{
   can_trigger = function(self, event, target, player, data)
     return player:hasSkill(self) and data.card.trueName == "slash" and player:distanceTo(player.room:getPlayerById(data.to)) <= 1
   end,
+  on_cost = function (self, event, target, player, data)
+    local prompt = (player.id == data.to) and "#kangkai-self" or "#kangkai-invoke::"..data.to
+    return player.room:askForSkillInvoke(player, self.name, nil, prompt)
+  end,
   on_use = function(self, event, target, player, data)
     local room = player.room
     local to = room:getPlayerById(data.to)
     player:drawCards(1, self.name)
-    if player == to then return end
+    if player == to or player:isNude() or to.dead then return end
     local cards = room:askForCard(player, 1, 1, true, self.name, false, ".", "#kangkai-give::"..to.id)
     if #cards > 0 then
       local card = Fk:getCardById(cards[1])
       room:moveCardTo(card, Card.PlayerHand, to, fk.ReasonGive, self.name, nil, true, player.id)
-      if card.type == Card.TypeEquip and not to.dead and not to:isProhibited(to, card) and
+      if card.type == Card.TypeEquip and not to.dead and not to:isProhibited(to, card) and not to:prohibitUse(card) and
         table.contains(to:getCardIds("h"), card.id) and
         room:askForSkillInvoke(to, self.name, data, "#kangkai-use:::"..card:toLogString()) then
         room:useCard({
@@ -1784,6 +1788,8 @@ Fk:loadTranslationTable{
   ["kangkai"] = "慷忾",
   [":kangkai"] = "每当一名角色成为【杀】的目标后，若你与其的距离不大于1，你可以摸一张牌，若如此做，你先将一张牌交给该角色再令其展示之，"..
   "若此牌为装备牌，其可以使用之。",
+  ["#kangkai-invoke"] = "慷忾：你可以摸一张牌，再交给 %dest 一张牌",
+  ["#kangkai-self"] = "慷忾：你可以摸一张牌",
   ["#kangkai-give"] = "慷忾：选择一张牌交给 %dest",
   ["#kangkai-use"] = "慷忾：你可以使用%arg",
 
